8f0f9592601112969b6658ae423b39d00fec78a8
Riley Moore <rilemoore@gmail.com>
2019-10-24 21:22:48 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,7 +7,13 @@ class Person
 
   def get_married(person)
     self.partner = person
-    person.partner = self
+    if person.class != 
+      raise PartnerError
+    else
+      Person.partner = self
+  end
+
+  class PartnerError < StandardError
   end
 
 end
